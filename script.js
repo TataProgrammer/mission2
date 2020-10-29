283268d34dfe47d5dccf5b534e688d7ecaab2a77
TataProgrammer <73477872+TataProgrammer@users.noreply.github.com>
2020-10-29 10:04:31 +0300

    first

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+
+var num = +prompt('Введите время в цифрах от 0 до 24');
+
+switch(num){
+    case 0:
+        alert('сейчас полночь');
+        break;
+    case 1:
+        alert('сейчас час ночи');
+        break;
+    case 2:
+        alert('сейчас два часа ночи');
+        break;
+    case 3:
+        alert('сейчас три часа ночи');
+        break;
+    case 4:
+        alert('сейчас четыре часа ночи');
+        break;
+    case 5:
+        alert('сейчас пять часов утра');
+        break;
+    case 6:
+        alert('сейчас шесть часов утра');
+        break;
+    case 7:
+        alert('сейчас семь часов утра');
+        break;
+    case 8:
+        alert('сейчас восемь часов утра');     
+        break;
+    case 9:
+        alert('чейчас девять часов утра');    
+        break;
+    case 10:
+        alert('сейчас десять часов утра');    
+        break; 
+    case 11:
+        alert('сейчас одиннадцать часов утра');
+        break;
+    case 12:
+        alert('сейчас дненадцать часов дня');
+        break;
+    case 13:
+        alert('сейчас час дня');
+        break;       
+    case 14:
+        alert('сейчас два часа дня');
+        break;  
+    case 15:
+        alert('сейчас три часа дня');    
+        break;  
+    case 16:
+        alert('сейчас четыре часа дня');    
+        break;  
+    case 17:
+        alert('сейчас пять часов вечера');    
+        break;  
+    case 18:
+        alert('сейчас шесть часов вечера');    
+        break;  
+    case 19:
+        alert('сейчас семь часов вечера');
+        break;
+    case 20:
+        alert('сейчас восемь часов вечера');      
+        break;  
+    case 21:
+        alert('сейчас девять часов вечера');    
+        break;  
+    case 22:
+        alert('сейчас десять часов вечера');    
+        break;  
+    case 23:
+        alert('сейчас одиннадцать часов вечра');    
+        break;  
+    case 24:
+        alert('сейчас полночь');    
+        break; 
+    default:
+        alert('Ошибка')     
+}
+
